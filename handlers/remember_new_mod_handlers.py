442c0aeb615e743e91083206842efded8c35b20f
TheWarmestThomas <reddreadthomaas@yandex.ru>
2020-06-16 22:21:29 +1000

    Исправил небольшой баг, вроде

--- handlers/remember_new_mod_handlers.py
+++ handlers/remember_new_mod_handlers.py
@@ -27,10 +27,10 @@ async def show_all_reminders(call):
     print(list_of_reminders)
     list_of_mods = reminder_mod_session.get_answer().split(' ')
     list_of_levels = reminder_level_session.get_answer().split(' ')
-    await call.message.answer(f"У вас активировано {len(list_of_dates)} нап:")
-    for i in range(len(list_of_dates)):
+    await call.message.answer(f"У вас активировано {len(list_of_dates) - 1} нап:")
+    for i in range(len(list_of_dates) - 1):
         #remained = (int(list_of_dates[i]) + logic.intervals_gen(int(list_of_mods[i]))[int(list_of_levels[i])] * 60 - int(time.time())) // 60
-        await call.message.answer(f"Тема: {list_of_reminders[i]}\n"
+        await call.message.answer(f"Тема: {list_of_reminders[i].replace('!@$%^&*()_+', ' ')}\n"
                                   f"Режим: {mods[int(list_of_mods[i])-1]}\n")
 
 
