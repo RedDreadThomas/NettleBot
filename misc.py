603c4a5bc0c67ad2dababb900799a0783d649ef7
TheWarmestThomas <reddreadthomaas@yandex.ru>
2020-06-16 15:18:14 +1000

    Добавлен storage в dp

--- misc.py
+++ misc.py
@@ -1,7 +1,9 @@
-import logging
-from aiogram import Bot, Dispatcher
 import config
+from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+
 
 bot = Bot(token=config.token)
-dp = Dispatcher(bot)
-logging.basicConfig(level=logging.INFO)
+dp = Dispatcher(bot, storage=MemoryStorage)
+dp.middleware.setup(LoggingMiddleware())
